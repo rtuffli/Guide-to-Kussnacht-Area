dbf8e6424fb585b143c642ab48b8492400af092b
rtuffli <72729248+rtuffli@users.noreply.github.com>
2025-09-14 14:01:50 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,226 @@
+window.POIS = [
+  {
+    "name": "Home Base --  Reto's Apartment (Jaistweg 6, Kussnacht am Rigi)",
+    "lat": 47.0883419696012,
+    "lng": 8.43477993154596,
+    "category": "red",
+    "description": "Enjoy your stay!  See users guide for details"
+  },
+  {
+    "name": "Farm Kiosk",
+    "lat": 47.086298776288,
+    "lng": 8.42965287605965,
+    "category": "green",
+    "description": "Get super fresh eggs & jam (honor system). Bring coins. It's a 5min walk from the apt."
+  },
+  {
+    "name": "Golf course (or just hit some balls)",
+    "lat": 47.0901943275501,
+    "lng": 8.43237218348815,
+    "category": "blue",
+    "description": "Driving range open to all.  Need handicap to play course"
+  },
+  {
+    "name": "Kussnacht town center (Dorfplatz)",
+    "lat": 47.0813976066635,
+    "lng": 8.44130583088118,
+    "category": "blue",
+    "description": "Center of town"
+  },
+  {
+    "name": "Martina Tuffli's childhood home",
+    "lat": 47.081205213357,
+    "lng": 8.44168478899524,
+    "category": "blue",
+    "description": "Her parents ran a grocery store (no longer exists)"
+  },
+  {
+    "name": "Coop - Grocery Store",
+    "lat": 47.082650400297,
+    "lng": 8.43977574224504,
+    "category": "green",
+    "description": "One of the two main grocery stores in town"
+  },
+  {
+    "name": "Migro - Grocery Store",
+    "lat": 47.0840172982129,
+    "lng": 8.44148536458731,
+    "category": "green",
+    "description": "One of the two main grocery stores in town"
+  },
+  {
+    "name": "Hotel Restaurant Seehof (Lakeside Dining)",
+    "lat": null,
+    "lng": null,
+    "category": "blue",
+    "description": "Have a bite or dessert on the lakeside terrace."
+  },
+  {
+    "name": "Strandbad Seeburg (Local Beach)",
+    "lat": 47.073615,
+    "lng": 8.433874,
+    "category": "blue",
+    "description": "Local beach with food and facilities; great on warm days."
+  },
+  {
+    "name": "Seebodenalp (favorite local hike)",
+    "lat": 47.0689616491004,
+    "lng": 8.4668011772483,
+    "category": "blue",
+    "description": "Short local hike (20 mins each way). Stop for a snack at Ruodisegg at end of hike.  Take aerial tram if no car."
+  },
+  {
+    "name": "Ruodisegg (end of short hike along Seebodenalp)",
+    "lat": 47.0577377374385,
+    "lng": 8.45340780245644,
+    "category": "blue",
+    "description": ""
+  },
+  {
+    "name": "Rigi (main local mountain and \"Queen of the Mountains\"",
+    "lat": 47.0566686222885,
+    "lng": 8.48507128092543,
+    "category": "blue",
+    "description": "This spot is the Rigi Kulm (summit). Great views.  Train goes all the way to here.  Restaurant and hotel there."
+  },
+  {
+    "name": "Areal tram to Seebodenalp",
+    "lat": 47.0761223902914,
+    "lng": 8.43989537364639,
+    "category": "blue",
+    "description": "Tram station in town"
+  },
+  {
+    "name": "Lucerne (Old Town, Chapel Bridge)",
+    "lat": 47.0502,
+    "lng": 8.3093,
+    "category": "blue",
+    "description": "Stroll Old Town, Chapel Bridge, Lion Monument, lakeside promenade. Easy train ride into town from Kussnacht"
+  },
+  {
+    "name": "Chapel Bridge",
+    "lat": 47.0515917543624,
+    "lng": 8.30746927377216,
+    "category": "blue",
+    "description": "Top landmark"
+  },
+  {
+    "name": "Bucherer Watch Store",
+    "lat": 47.0534426865594,
+    "lng": 8.30837869871546,
+    "category": "blue",
+    "description": "Flagship store. Five floors of expensive watches"
+  },
+  {
+    "name": "Swiss Museum of Transport (Verkehrshaus)",
+    "lat": 47.0525287,
+    "lng": 8.3355776,
+    "category": "blue",
+    "description": "Rainy day favorite with IMAX; easy by train/boat."
+  },
+  {
+    "name": "Lake Lucerne Boat Ride (Lucerne piers)",
+    "lat": 47.0512849459965,
+    "lng": 8.31077973967951,
+    "category": "blue",
+    "description": "Best to start from Lucerne piers for most options; classic paddle steamers."
+  },
+  {
+    "name": "Mount Rigi (Vitznau cogwheel start)",
+    "lat": 47.0104,
+    "lng": 8.4761,
+    "category": "blue",
+    "description": "Rigi Bahnen cogwheel railway starts here."
+  },
+  {
+    "name": "Mount Rigi (Weggis cable car)",
+    "lat": 47.0340845171802,
+    "lng": 8.44071841761377,
+    "category": "blue",
+    "description": "Aerial tram from Weggis up to Rigi Kaltbad."
+  },
+  {
+    "name": "Mount Pilatus (Pilatus Kulm)",
+    "lat": 46.979,
+    "lng": 8.253,
+    "category": "blue",
+    "description": "Worldâ€™s steepest cogwheel railway or aerial tram; epic views."
+  },
+  {
+    "name": "Burgenstock (chic resort for well heeled / celebrities)",
+    "lat": null,
+    "lng": null,
+    "category": "blue",
+    "description": "Great views.  Great hike along cliff to "
+  },
+  {
+    "name": "Hammetschwand Elevator",
+    "lat": 47.0015006246576,
+    "lng": 8.39627617347001,
+    "category": "blue",
+    "description": "On the hike along clif from Burgenstock.  Great views.  Optionally walk back down to Burgenstock"
+  },
+  {
+    "name": "Stoos Ridge Hike (Klingenstock to Fronalpstock)",
+    "lat": 46.9789,
+    "lng": 8.6684,
+    "category": "blue",
+    "description": "Steep funicular to Stoos, chairlift, 2 hour ridge hike to Fronalpstock, chairlift down."
+  },
+  {
+    "name": "Start place for Stoos (steepest funicular)",
+    "lat": null,
+    "lng": null,
+    "category": "",
+    "description": ""
+  },
+  {
+    "name": "Klingenstock (start of hike)",
+    "lat": 46.9569232276736,
+    "lng": 8.67594085941027,
+    "category": "",
+    "description": "Take lift up to start hike"
+  },
+  {
+    "name": "Fronalpstock (end of hike)",
+    "lat": 46.9696219985417,
+    "lng": 8.63741024906172,
+    "category": "",
+    "description": "Take lift down"
+  },
+  {
+    "name": "Hotel Villa Honegg (20 min hike from Burgenstock)",
+    "lat": 46.9951269135914,
+    "lng": 8.4028306154636,
+    "category": "blue",
+    "description": "Beautiful views.  Have a dessert on the back terrace. Check out the outdoor pool"
+  },
+  {
+    "name": "Jungfrau Region - Lauterbrunnen",
+    "lat": 46.593,
+    "lng": 7.9076,
+    "category": "blue",
+    "description": "Waterfalls & valley views; gateway to Wengen / Murren"
+  },
+  {
+    "name": "Jungfrau Region - Murren",
+    "lat": 46.562,
+    "lng": 7.8921,
+    "category": "blue",
+    "description": "Clifftop village with big views."
+  },
+  {
+    "name": "Jungfrau Region - Grindelwald",
+    "lat": 46.6242,
+    "lng": 8.0414,
+    "category": "blue",
+    "description": "Base for Eiger views and mountain excursions."
+  },
+  {
+    "name": "Jungfrau Region - Wengen",
+    "lat": 46.6068,
+    "lng": 7.9217,
+    "category": "blue",
+    "description": "Car-free village perched above the valley."
+  }
+];
